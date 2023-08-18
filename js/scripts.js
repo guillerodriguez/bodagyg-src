69f20cc1537d330a8b3c96da13dbaba1d62b1b39
Guillermo Rodríguez <grodriguez@ingelabs.com>
2023-08-18 21:40:56 +0200

    Sección Cuándo y cambios en sección Dónde

--- js/scripts.js
+++ js/scripts.js
@@ -180,23 +180,20 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Boda G y G",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('Nov 4, 2023 12:00'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('Nov 5, 2023 3:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
-
-            // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            address: 'Hotel B Bou La Viñuela',
         }
     });
 
